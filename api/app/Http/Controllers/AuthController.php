a916b73fa8a30ee6ba757150ff13fe4497daaf98
kingrayhan <rayhan095@gmail.com>
2021-10-08 21:31:08 +0600

    ep2

---
+++ api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+
+use App\Models\User;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+
+    /**
+     * Register new user
+     * @param RegisterRequest $request
+     * @return string[]
+     */
+    public function register(RegisterRequest $request)
+    {
+
+        User::create($request->all());
+
+        return [
+            'message' => 'Registered successfully',
+        ];
+
+    }
+
+
+    public function login(LoginRequest $request)
+    {
+        if(!auth()->attempt($request->only('email', 'password'))){
+            throw new AuthenticationException();
+        }
+
+
+        $token = auth()->user()->createToken('user-token');
+
+        return [
+            'message' => ['successfully logged in'],
+            'token' => $token->plainTextToken
+        ];
+    }
+
+    public function logout()
+    {
+        \auth()->user()->currentAccessToken()->delete();
+
+        return [
+            'message' => 'Successfully logged out',
+        ];
+    }
+
+    /**
+     * Get authenticated user
+     */
+    public function user()
+    {
+        return auth()->user();
+    }
+}
---
+++ api/routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['prefix' => 'auth'], function (){
+   Route::post('register', [AuthController::class, 'register']);
+   Route::post('login', [AuthController::class, 'login']);
+   Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');;
+   Route::get('user', [AuthController::class, 'user'])->middleware('auth:sanctum');
+});
+
+
+
+
+
+
+// api/auth/register
+// login
+// logout
